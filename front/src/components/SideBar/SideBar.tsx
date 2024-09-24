69e401b2dde2080bf46304689c2812614e5cced5
OsvaldoComCH <eduhrsilva@gmail.com>
2024-09-24 14:15:08 -0300

    tela operação

--- front/src/components/SideBar/SideBar.tsx
+++ front/src/components/SideBar/SideBar.tsx
@@ -1,21 +1,31 @@
+import { Link } from 'react-router-dom';
+
 const SideBar = () => {
   return (
-    <aside className="bg-white h-screen w-28 shadow-lg">
-      <div className="flex flex-col items-center justify-center h-full p-4">
-      <img src="images/digiexfundo.png" alt="Logo" />
-        <ul className="mt-4 space-y-4">
+    <aside className="bg-white h-screen w-28 shadow-lg flex flex-col">
+      <div className="pt-4">
+        <Link to="/">
+          <img src="images/digiexfundo.png" alt="Logo" />
+        </Link>
+      </div>
+      {/* Espaço vazio para empurrar os botões para o meio */}
+      <div className="flex-grow flex items-center justify-center">
+        <ul className="space-y-7">
           <li>
-            <button className="flex items-center justify-center w-full h-10 bg-transparent hover:bg-gray-200 rounded py-10">
-              <img src="images/home.svg" alt="Home" />
-            </button>
+            {/* esse link faz a logo retornar para a home */}
+            <Link to="/"> 
+              <button className="pb-10 flex items-center justify-center w-full h-10 bg-transparent hover:bg-gray-200 rounded">
+                <img src="images/home.svg" alt="Home" />
+              </button>
+            </Link>
           </li>
           <li>
-            <button className="flex items-center justify-center w-full h-10 bg-transparent hover:bg-gray-200 rounded py-10">
+            <button className="py-10 flex items-center justify-center w-full h-10 bg-transparent hover:bg-gray-200 rounded">
               <img src="images/circle.svg" alt="Circle" />
             </button>
           </li>
           <li>
-            <button className="flex items-center justify-center w-full h-10 bg-transparent hover:bg-gray-200 rounded py-10">
+            <button className="py-10 flex items-center justify-center w-full h-10 bg-transparent hover:bg-gray-200 rounded">
               <img src="images/cart.svg" alt="Cart" />
             </button>
           </li>
--- front/src/routes/mainRoutes.tsx
+++ front/src/routes/mainRoutes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { Home } from "../pages/home/home";
+import OperationPage from "../pages/OperationPage";
 
 const MainRoutes = createBrowserRouter([
     {
@@ -8,6 +9,13 @@ const MainRoutes = createBrowserRouter([
         element: 
         <App>
             <Home/>
+        </App>
+    },
+    {
+        path: "operation",
+        element:
+        <App>
+            <OperationPage/>
         </App>
     }
 ])
