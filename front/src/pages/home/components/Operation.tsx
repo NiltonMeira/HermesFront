144ecdb6f3a15d55ac9b7f11b8eb79ab4a9b4502
Nilton <niltonprudys@gmail.com>
2024-09-25 19:59:57 -0300

    feat: home page

---
+++ front/src/pages/home/components/Operation.tsx
@@ -0,0 +1,9 @@
+export const Operation = () => {
+    <div className="flex">
+        <div className="flex">
+            <h1>Entrada</h1>
+            <img src="images/carrinho.png" alt="" />
+        </div>
+
+    </div>
+}
--- front/src/pages/home/home.tsx
+++ front/src/pages/home/home.tsx
@@ -1,7 +1,16 @@
+import Bar from "../../components/Bar/Bar";
+import SideBar from "../../components/SideBar/SideBar";
+
 export const Home = () => {
     return(
-        <>
-        <p>uhiyigiyg </p>
-        </>
-    )
+            <div className="flex flex-col h-screen">
+                <Bar/>
+                <div className="flex flex-grow">
+                    <SideBar />
+                    <main className="flex-grow p-4">
+                        
+                    </main>
+                </div>
+            </div>
+        );
 }
